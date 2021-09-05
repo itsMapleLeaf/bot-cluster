8989a23bab00b562a8d48c19e61e8784bbdbfe81
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-05 16:48:50 -0500

    rollbot: convert to TS + improve input handling

---
+++ bots/rollbot/src/main.ts
@@ -0,0 +1,15 @@
+// @ts-check
+import { createGatekeeper } from "@itsmapleleaf/gatekeeper"
+import { Client, Intents } from "discord.js"
+import "dotenv/config.js"
+import { rollCommand } from "./roll"
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+})
+
+const gatekeeper = createGatekeeper({ debug: true })
+gatekeeper.addCommand(rollCommand)
+gatekeeper.useClient(client)
+
+client.login(process.env.ROLLBOT_BOT_TOKEN).catch(console.error)
